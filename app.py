b990300f1c7a0e7b9d9378c1138a91249eddc1db
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-12-16 19:38:24 -0600

    Added user input for values and changed them to lowercase to prevent input errors. Updated Comments and changed what values were returned to the console.

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from src.metrotransit_api import MetroTransitAPI
 API = MetroTransitAPI()
 
-route = "Brklyn Center - Fremont - 26th Av - Chicago - MOA"
-direction = 4
-stop = "Portland Ave and 77th St"
+# gets users input to input into the MetroTransitAPI class
+route = input("Please enter in a route name: ").lower()
+direction = int(input("Please enter in a direction number: "))
+stop = input("Please enter in a stop name: ").lower()
+
+# sets the returned input from MetroTransitAPI to variables
+# those variables are then input into the API for the next departure time
+# the final returned value is returned
 route_number = API.get_route_number(route)
-print(route_number)
 stop_id = API.get_stop_identifier(stop, direction, route_number)
-print(stop_id)
-departure_time = API.get_next_departure(stop_id, route_number, direction)
+
+# gets the returned time, if applicable, and then prints it
+departure_time = API.get_next_departure(route_number, direction, stop_id)
 print(departure_time)
 
--- src/metrotransit_api.py
+++ src/metrotransit_api.py
@@ -16,19 +16,24 @@ class MetroTransitAPI(object):
         read_url = self.read_provided_url(routes_url)
         parse_read_url = self.parse_provided_url(read_url)
         for items in parse_read_url:
-            if route in items['Description']:
+            if route in items['Description'].lower():
                 return items['Route']
             else:
                 pass
 
+    # defines the get_stop_identifier which intakes a bus stop name, direction number, and route number
+    # the function then returns the bus stop number if applicable
     def get_stop_identifier(self, bus_stop, direction, route_number):
         stops_url = DEFAULT_URL + "Stops/" + str(route_number) + "/" + str(direction) + JSON_FORMAT
         read_url = self.read_provided_url(stops_url)
         parsed_url = self.parse_provided_url(read_url)
         for stops in parsed_url:
-            if bus_stop in stops['Text']:
+            if bus_stop in stops['Text'].lower():
                 return stops['Value']
 
+    # defines_get_departure which takes in a route number, direction, and stop id
+    # the function then loops through a json for the first 'DepartureText'
+    # this value is the time of departure
     def get_next_departure(self, route_number, direction, stop_id):
         departure_time_url = DEFAULT_URL + str(route_number) + "/" + str(direction) + "/" + str(stop_id) + JSON_FORMAT
         read_url = self.read_provided_url(departure_time_url)
@@ -36,10 +41,12 @@ class MetroTransitAPI(object):
         for times in parsed_url:
             return times['DepartureText']
 
+    # defines a function to read a url provided
     def read_provided_url(self, provided_url):
         read_url = request.urlopen(provided_url).read().decode('utf-8')
         return read_url
 
-    def parse_provided_url(self, provided_url):
-        parsed_url = json.loads(provided_url)
+    # defines a function that takes a read url and parses it
+    def parse_provided_url(self, read_url):
+        parsed_url = json.loads(read_url)
         return parsed_url
